255066e849162e3f6ae5decc929710215196671b
Vladislav Didukh <elitniistalker@gmail.com>
2020-06-07 01:05:36 +0300

    another try to fix path

--- script/script.js
+++ script/script.js
@@ -28,23 +28,23 @@ function startup() {
 		confirmBtn.classList.add("confirm-text-style");
 	}
 
-	// let index = 0;
-	// let arrayOfPhotos = [
-	// 	"../images/Sea.jpg",
-	// 	"../images/Sea1.jpg",
-	// 	"../images/Sea2.jpg",
-	// 	"../images/Sea3.jpg",
-	// 	"../images/Sea4.jpg",
-	// ];
-	// let title = document.querySelector(".title");
-	// let sliderStatus = document.querySelectorAll(".slider-move");
+	let index = 1;
+	let arrayOfPhotos = [
+		"images/Sea.jpg",
+		"images/Sea1.jpg",
+		"images/Sea2.jpg",
+		"images/Sea3.jpg",
+		"images/Sea4.jpg",
+	];
+	let title = document.querySelector(".title");
+	let sliderStatus = document.querySelectorAll(".slider-move");
 
-	// setInterval(function () {
-	// 	if (index == arrayOfPhotos.length) {
-	// 		index = 1;
-	// 	}
+	setInterval(function () {
+		if (index == arrayOfPhotos.length) {
+			index = 1;
+		}
 
-	// 	index += 1;
-	banner.style.backgroundImage = `url(../images/Sea2.jpg)`;
-	// 	}, 2500);
+		index += 1;
+		banner.style.backgroundImage = `url(${arrayOfPhotos[index]})`;
+	}, 2500);
 }
