b0d98256b7102577d6cb589664c46c73334359f7
Vladislav Didukh <elitniistalker@gmail.com>
2020-06-06 19:28:14 +0300

    created slider

---
+++ script/script.js
@@ -0,0 +1,55 @@
+window.addEventListener("resize", changeIcon);
+document.addEventListener("DOMContentLoaded", startup);
+
+const banner = document.querySelector(".banner");
+const confirmBtn = document.querySelector("[data-find=confirm]");
+
+function changeIcon() {
+	let windowWidth = window.innerWidth;
+	if (windowWidth <= 768) {
+		if (!confirmBtn.classList.contains("confirm-text-style")) {
+			confirmBtn.textContent = "Confirm";
+			confirmBtn.classList.remove("confirm");
+			confirmBtn.classList.add("confirm-text-style");
+		}
+	} else {
+		if (confirmBtn.classList.contains("confirm-text-style")) {
+			confirmBtn.textContent = "";
+			confirmBtn.classList.remove("confirm-text-style");
+			confirmBtn.classList.add("confirm");
+		}
+	}
+}
+
+function startup() {
+	if (window.matchMedia("(max-width: 768px)").matches) {
+		confirmBtn.textContent = "Confirm";
+		confirmBtn.classList.remove("confirm");
+		confirmBtn.classList.add("confirm-text-style");
+	}
+
+	let index = 0;
+	let arrayOfPhotos = [
+		"../images/Sea.jpg",
+		"../images/Sea1.jpg",
+		"../images/Sea2.jpg",
+		"../images/Sea3.jpg",
+		"../images/Sea4.jpg",
+	];
+	let title = document.querySelector(".title");
+	let sliderStatus = document.querySelectorAll(".slider-move");
+
+	setInterval(function () {
+		if (index == arrayOfPhotos.length - 1) {
+			index = 0;
+		}
+		if (arrayOfPhotos[index] == "../images/Sea1.jpg") {
+			title.style.textShadow = "1px 1px 2px #000";
+		} else {
+			title.style.textShadow = "none";
+		}
+
+		index += 1;
+		banner.style.backgroundImage = `url(${arrayOfPhotos[index]})`;
+	}, 2500);
+}
