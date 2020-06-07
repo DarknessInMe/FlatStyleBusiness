298a7df9fa2edde96038a74c03bbcbba6de3dac5
Vladislav Didukh <elitniistalker@gmail.com>
2020-06-07 20:55:21 +0300

    optimized images size. Removed a slider effect for mobile phones

--- script/script.js
+++ script/script.js
@@ -1,50 +1,56 @@
-window.addEventListener("resize", changeIcon);
-window.addEventListener("load", startup);
+window.addEventListener("load", checkMedia);
 
 const banner = document.querySelector(".banner");
 const confirmBtn = document.querySelector("[data-find=confirm]");
+const screen = window.matchMedia("(max-width: 768px)");
 
-function changeIcon() {
-	let windowWidth = window.innerWidth;
-	if (windowWidth <= 768) {
-		if (!confirmBtn.classList.contains("confirm-text-style")) {
-			confirmBtn.textContent = "Confirm";
-			confirmBtn.classList.remove("confirm");
-			confirmBtn.classList.add("confirm-text-style");
-		}
+let toggle = true;
+
+function checkMedia() {
+	screen.addListener(buttonTransform);
+	buttonTransform(screen);
+}
+
+function buttonTransform(screen) {
+	if (screen.matches) {
+		confirmBtn.textContent = "Confirm";
+		confirmBtn.classList.remove("confirm");
+		confirmBtn.classList.add("confirm-text-style");
+
+		toggle = false;
+		banner.style.backgroundImage = "url(images/banner/sea_mobile.jpg)";
 	} else {
-		if (confirmBtn.classList.contains("confirm-text-style")) {
-			confirmBtn.textContent = "";
-			confirmBtn.classList.remove("confirm-text-style");
-			confirmBtn.classList.add("confirm");
-		}
+		confirmBtn.textContent = "";
+		confirmBtn.classList.remove("confirm-text-style");
+		confirmBtn.classList.add("confirm");
+
+		toggle = true;
+		startup();
 	}
 }
 
 function startup() {
-	if (window.matchMedia("(max-width: 768px)").matches) {
-		confirmBtn.textContent = "Confirm";
-		confirmBtn.classList.remove("confirm");
-		confirmBtn.classList.add("confirm-text-style");
-	}
-
 	let index = 1;
 	let arrayOfPhotos = [
-		"images/Sea.jpg",
-		"images/Sea1.jpg",
-		"images/Sea2.jpg",
-		"images/Sea3.jpg",
-		"images/Sea4.jpg",
+		"images/banner/Sea1.jpg",
+		"images/banner/Sea2.jpg",
+		"images/banner/Sea3.jpg",
+		"images/banner/Sea4.jpg",
+		"images/banner/Sea5.jpg",
 	];
+
 	let title = document.querySelector(".title");
 	let sliderStatus = document.querySelectorAll(".slider-move");
 
-	setInterval(function () {
-		if (index == arrayOfPhotos.length) {
-			index = 1;
+	const changes = setInterval(function () {
+		if (!toggle) {
+			clearTimeout(changes);
+		} else {
+			if (index == arrayOfPhotos.length) {
+				index = 0;
+			}
+			banner.style.backgroundImage = `url(${arrayOfPhotos[index]})`;
+			index += 1;
 		}
-
-		index += 1;
-		banner.style.backgroundImage = `url(${arrayOfPhotos[index]})`;
 	}, 2500);
 }
