16c42c40894f114a807baac7e0321345ded7ae42
Vladislav Didukh <elitniistalker@gmail.com>
2020-06-09 22:33:08 +0300

    Removed dynamic background changing between mobile and desktop modes. It was desided as useless feature, which creates an extra load

--- script/script.js
+++ script/script.js
@@ -1,37 +1,29 @@
-window.addEventListener("load", checkMedia);
-
 const banner = document.querySelector(".banner");
 const confirmBtn = document.querySelector("[data-find=confirm]");
 const screen = window.matchMedia("(max-width: 768px)");
 
-let toggle = true;
-
-function checkMedia() {
+(function checkMedia() {
 	screen.addListener(buttonTransform);
 	buttonTransform(screen);
-}
+})();
 
 function buttonTransform(screen) {
 	if (screen.matches) {
 		confirmBtn.textContent = "Confirm";
 		confirmBtn.classList.remove("confirm");
 		confirmBtn.classList.add("confirm-text-style");
-
-		toggle = false;
-		banner.style.backgroundImage = "url(images/banner/sea_mobile.jpg)";
 	} else {
 		confirmBtn.textContent = "";
 		confirmBtn.classList.remove("confirm-text-style");
 		confirmBtn.classList.add("confirm");
 
-		toggle = true;
 		startup();
 	}
 }
 
 function startup() {
 	let index = 1;
-	let arrayOfPhotos = [
+	const arrayOfPhotos = [
 		"images/banner/Sea1.jpg",
 		"images/banner/Sea2.jpg",
 		"images/banner/Sea3.jpg",
@@ -43,14 +35,10 @@ function startup() {
 	let sliderStatus = document.querySelectorAll(".slider-move");
 
 	const changes = setInterval(function () {
-		if (!toggle) {
-			clearTimeout(changes);
-		} else {
-			if (index == arrayOfPhotos.length) {
-				index = 0;
-			}
-			banner.style.backgroundImage = `url(${arrayOfPhotos[index]})`;
-			index += 1;
+		if (index == arrayOfPhotos.length) {
+			index = 0;
 		}
+		banner.style.backgroundImage = `url(${arrayOfPhotos[index]})`;
+		index += 1;
 	}, 2500);
 }
