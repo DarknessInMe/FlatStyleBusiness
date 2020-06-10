f037b875ea37e5081591a63b4b0a81b247397704
Vladislav Didukh <elitniistalker@gmail.com>
2020-06-10 18:17:42 +0300

    fixed some bugs: a visible border-top of hidden burger menu on mobile phones, returned smooth transition between mobile and desktop slider modes

--- script/script.js
+++ script/script.js
@@ -1,18 +1,18 @@
 const SCREEN = window.matchMedia("(max-width: 768px)");
+const ARRAY_OF_PHOTOS = [
+	"images/banner/Sea1.jpg",
+	"images/banner/Sea2.jpg",
+	"images/banner/Sea3.jpg",
+	"images/banner/Sea4.jpg",
+	"images/banner/Sea5.jpg",
+];
 
 window.onload = function () {
 	SCREEN.addListener(buttonTransform);
 	buttonTransform(SCREEN);
 	if (!SCREEN.matches) {
-		let arrayOfPhotos = preloadImages(
-			"images/banner/Sea1.jpg",
-			"images/banner/Sea2.jpg",
-			"images/banner/Sea3.jpg",
-			"images/banner/Sea4.jpg",
-			"images/banner/Sea5.jpg"
-		);
-
-		startSlider(arrayOfPhotos);
+		preloadImages(...ARRAY_OF_PHOTOS);
+		startSlider();
 	}
 };
 
@@ -21,7 +21,6 @@ function preloadImages(...bgs) {
 		image = document.createElement("img");
 		image.src = bgs[index];
 	});
-	return bgs;
 }
 
 document.querySelector(".menu-burger").onclick = function () {
@@ -34,6 +33,8 @@ const CONFIRM_BTN = document.querySelector("[data-find=confirm]");
 const QUOTE_BLOCK = document.querySelector(".quote-block");
 const HIDDEN_NAME = document.querySelector("#hidden-name");
 
+let toggleSlider = true;
+
 function buttonTransform(SCREEN) {
 	if (SCREEN.matches) {
 		CONFIRM_BTN.textContent = "Confirm";
@@ -43,6 +44,8 @@ function buttonTransform(SCREEN) {
 		QUOTE_BLOCK.style.flexDirection = "column";
 		HIDDEN_NAME.textContent = "dhavan";
 		HIDDEN_NAME.classList.add("_name-modified");
+
+		toggleSlider = false;
 	} else {
 		CONFIRM_BTN.textContent = "";
 		CONFIRM_BTN.classList.remove("confirm-text-style");
@@ -51,25 +54,35 @@ function buttonTransform(SCREEN) {
 		QUOTE_BLOCK.style.flexDirection = "row";
 		HIDDEN_NAME.textContent = "";
 		HIDDEN_NAME.classList.remove("_name-modified");
+
+		toggleSlider = true;
+		startSlider();
 	}
 }
 
-function startSlider(arrayOfPhotos) {
+function startSlider() {
 	let index = 1;
 
 	let sliderStatus = document.querySelectorAll(".slider-move");
 	sliderStatus[0].classList.add("_selected");
 
-	const changes = setInterval(function () {
-		if (index == arrayOfPhotos.length) {
+	const SLIDER_INTERVAL = setInterval(function () {
+		if (!toggleSlider) {
+			BANNER.style.backgroundImage = `url(images/banner/sea_mobile.jpg)`;
+			clearInterval(SLIDER_INTERVAL);
+			dropSliderStatus(sliderStatus);
 			index = 0;
+		} else {
+			if (index == ARRAY_OF_PHOTOS.length) {
+				index = 0;
+			}
+			dropSliderStatus(sliderStatus);
+			sliderStatus[index].classList.add("_selected");
+
+			BANNER.style.backgroundImage = `url(${ARRAY_OF_PHOTOS[index]})`;
+
+			index += 1;
 		}
-		dropSliderStatus(sliderStatus);
-		sliderStatus[index].classList.add("_selected");
-
-		BANNER.style.backgroundImage = `url(${arrayOfPhotos[index]})`;
-
-		index += 1;
 	}, 2500);
 }
 
